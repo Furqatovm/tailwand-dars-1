33459bf1f01522f58c1f70b5999c6b3456f641ae
Furqatovm <muhammadyusuffurqatov91@gmail.com>
2025-06-07 10:55:03 +0500

    the second page is readdy

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ searchch.addEventListener("click", function () {
     openn.classList.toggle("hidden");
     searchch.style.display ="block";
 });
+
+
+
+
+let menuu =document.getElementById("openn");
+let navv =document.getElementById("navv");
+
+menuu.addEventListener("click", function (){
+    navv.style.display ="flex";
+    navv.style.flexDirection ="column";
+    navv.style.gap ="1rem";
+    navv.style.width ="70vw";
+    navv.style.height ="100vh";
+    navv.style.backgroundColor ="#494465";
+    navv.style.position ="fixed";
+    navv.style.top ="0";
+    navv.style.right ="0";
+    navv.style.alignItems ="center";
+    navv.style.justifyContent ="center"
+})
+
+let closee =document.getElementById("close");
+closee.addEventListener("click", function(){
+    navv.style.display ="none";
+})
